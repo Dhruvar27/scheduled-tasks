f2d6b5a9b2e625334bf14f3be5bf19167c61aca6
Dhruvar27 <73665816+Dhruvar27@users.noreply.github.com>
2026-09-09 01:08:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import os
 
-import smtplib
+from smtplib import SMTP
 from calendar import week, weekday
 
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
-
-
 
 
 import datetime as dt
@@ -15,21 +13,25 @@ import random
 
 
 
-now = dt.datetime.now()
-current_weekday = now.weekday()
+today = dt.datetime.now()
+month = today.month
+day = today.day
+file_path = f"./letter_templates/letter_{random.randint(1, 3)}.txt"
+import pandas as pd
+df = pd.read_csv("birthdays.csv")
+for index, row in df.iterrows():
+    if row.day == day and row.month == month:
+        with open(file_path, mode="r") as letter:
+            letter_contents = letter.read()
 
-if current_weekday == 1:
+    # Now .replace() will work perfectly!
+            customised_letter = letter_contents.replace("[NAME]", row["name"].title())
+            final_letter = customised_letter.replace("Angela", "Dhruv")
 
-    with open("quotes.txt") as quotes:
-        quotes_list = quotes.read().splitlines()
-        random_quote = quotes_list[random.randint(1,100)]
-
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-
-        connection.starttls()
-        connection.login(user=my_email, password=password)
-        connection.sendmail(
-            from_addr=my_email,
-            to_addrs=receiver_email,
-            msg=f"Subject: New Quote! \n\n{random_quote}"
-    )
+            with SMTP("smtp.gmail.com") as connection:
+                connection.starttls()
+                connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+                connection.sendmail(
+                    from_addr=MY_EMAIL,
+                    to_addrs=row.email,
+                    msg=f"Subject: Happy birthday! \n\n{final_letter}")
